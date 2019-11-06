a1acdd24044294c572c6d65ea62511e72dce0808
Aidan <abchisum@gmail.com>
2019-11-06 12:11:24 -0800

    Fix for db issues
    A missing space messed up the table name. Deleted old database with bad table and remade it with working table with correct name. Ready to add values too!

--- app/src/main/java/com/example/closetvalue/ClosetDBHelper.java
+++ app/src/main/java/com/example/closetvalue/ClosetDBHelper.java
@@ -18,6 +18,8 @@ public class ClosetDBHelper extends SQLiteOpenHelper {
 
     public ClosetDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        Log.i(TAG, " DBHelper created");
+        
     }
 
     @Override
@@ -27,21 +29,31 @@ public class ClosetDBHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+//        db.execSQL(SQL_DELETE_ENTRIES);
+        Log.i(TAG, " Upgrading from " + i + " to " + i1);
+    }
 
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+//        db.execSQL(SQL_DELETE_ENTRIES);
+        Log.i(TAG, " Downgrading from " + oldVersion + " to " + newVersion);
     }
 
     private static final String CREATE_GARMENTS_TABLE =
-            "CREATE TABLE " + ClosetContract.Garment.TABLE_NAME + "(" +
-            ClosetContract.Garment._ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            ClosetContract.Garment.COLUMN_NAME_NAME +  "TEXT, " +
-            ClosetContract.Garment.COLUMN_NAME_TYPE + "TEXT, " +
-            ClosetContract.Garment.COLUMN_NAME_USES + "INTEGER, " +
-            ClosetContract.Garment.COLUMN_NAME_PRICE + "REAL, " +
-            ClosetContract.Garment.COLUMN_NAME_COLOR + "TEXT, " +
-            ClosetContract.Garment.COLUMN_NAME_SIZE + "TEXT, " +
-            ClosetContract.Garment.COLUMN_NAME_NOTES + "TEXT," +
+            "CREATE TABLE " + ClosetContract.Garment.TABLE_NAME + "( " +
+            ClosetContract.Garment._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            ClosetContract.Garment.COLUMN_NAME_NAME +  " TEXT, " +
+            ClosetContract.Garment.COLUMN_NAME_TYPE +  " TEXT, " +
+            ClosetContract.Garment.COLUMN_NAME_USES + " INTEGER, " +
+            ClosetContract.Garment.COLUMN_NAME_PRICE + " REAL, " +
+            ClosetContract.Garment.COLUMN_NAME_COLOR + " TEXT, " +
+            ClosetContract.Garment.COLUMN_NAME_SIZE + " TEXT, " +
+            ClosetContract.Garment.COLUMN_NAME_NOTES + " TEXT," +
             ClosetContract.Garment.COLUMN_NAME_SLEEVE_LENGTH + " Text, " +
-            ClosetContract.Garment.COLUMN_NAME_LEG_LENGTH + "Text" +
+            ClosetContract.Garment.COLUMN_NAME_LEG_LENGTH + " Text" +
             ")";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + ClosetContract.Garment.TABLE_NAME;
 }
